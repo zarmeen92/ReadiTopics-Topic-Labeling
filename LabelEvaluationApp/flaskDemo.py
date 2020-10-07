6c1849fd03ce4d24b1c9875238f853770f6bcac7
zarmeen92 <Zarmeen@phd-research-02.iba.edu.pk>
2020-10-07 16:51:37 +0500

    update

--- LabelEvaluationApp/flaskDemo.py
+++ LabelEvaluationApp/flaskDemo.py
@@ -13,14 +13,22 @@ app = Flask(__name__)
 
 dataset = pd.read_csv("all_labels_comparison_with_details.csv",sep='\t',encoding='utf-8')
 cluster_lbl= dataset['Cluster']
+column_names = ["Cluster", "Zero-Order", "M-Order","T-Order","ProposedMethod"]
+
+rating = pd.DataFrame(columns = column_names)
 @app.route('/')
+def home():
+	
+	return render_template('home.html')
+
+@app.route('/index')
 def index():
 	row = dataset.iloc[0]
 	top10terms = row['Top10Terms'].split(' ')
 	docids = range(0,5)
 	top5docs = row['Top5Docs'].split('$')
 	return render_template('index.html',
-						   title='Home',clusterid='0',
+						   title='Home',clusterid='0',row=row,
 						   top10terms=top10terms,top5docs=zip(docids,top5docs),
 						   text = '',cluster_lbl= cluster_lbl,time_exe = '')
 
@@ -33,9 +41,33 @@ def cluster_details(clusterid):
 	docids = range(0,5)
 	top5docs = row['Top5Docs'].split('$')
 	return render_template('index.html',
-						   title='Home',clusterid=clusterid,
+						   title='Home',clusterid=clusterid,row=row,
 						   top10terms=top10terms,top5docs=zip(docids,top5docs),
 						   text = '',cluster_lbl= cluster_lbl,time_exe = '')
+@app.route('/submitdata', methods=['POST'])
+def submitdata():
+		current_id=int(request.form['clusterid_current'])
+		zero_order = [request.form['completeness_method1'],request.form['relevance_method1'],request.form['correctness_method1']]
+		m_order = [request.form['completeness_method2'],request.form['relevance_method2'],request.form['correctness_method2']]
+		t_order = [request.form['completeness_method3'],request.form['relevance_method3'],request.form['correctness_method3']]
+		proposed_method = [request.form['completeness_method4'],request.form['relevance_method4'],request.form['correctness_method4']]
+		nextid = int(request.form['clusterid_current']) + 1
+		rating.loc[current_id] = [current_id,";".join(zero_order),";".join(m_order),";".join(t_order),";".join(proposed_method)]
+		print(rating)
+		if nextid>=13:
+			unique_filename = str(uuid.uuid4()) 
+			rating.to_csv(unique_filename + '.csv', sep='\t',index=False,encoding='utf-8')
+			return render_template('thankyou.html')
+		else:
+			row = dataset.iloc[int(nextid)]
+			top10terms = row['Top10Terms'].split(' ')
+			docids = range(0,5)
+			top5docs = row['Top5Docs'].split('$')
+			
+			return render_template('index.html',
+							   title='Home',clusterid=nextid,row=row,
+							   top10terms=top10terms,top5docs=zip(docids,top5docs),
+							   text = '',cluster_lbl= cluster_lbl,time_exe = '')
 
 def allowed_file(filename):
     return '.' in filename and \
